1a9d49f2052bf8b3cb91d0c463c812e4289272c1
Nicholas Barnes <n3ttl3t@gmail.com>
2019-05-07 16:41:45 +0100

    Implemented maximum limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,22 @@
 class Oystercard
-  attr_reader :balance
+  attr_reader :balance, :DEFAULT_MAXIMUM
 
   DEFAULT_BALANCE = 0
+  DEFAULT_MAXIMUM = 90
 
   def initialize
     @balance = DEFAULT_BALANCE
   end
 
   def top_up(amount)
+    raise "Balance can't be more than #{DEFAULT_MAXIMUM}" if maximum_exceeded?(amount)
     @balance += amount
   end
+
+  private
+
+  def maximum_exceeded?(amount)
+    @balance + amount > DEFAULT_MAXIMUM
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,16 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:card) { Oystercard.new }
+
   it 'has 0 balance by default' do
-    card = Oystercard.new
     expect(card.balance).to eq(0)
   end
 
   it 'should respond to top_up with one argument' do
-    card = Oystercard.new
-    #expect(card).to respond_to(:top_up).with(1).argument
     card.top_up(10)
     expect(card.balance).to eq(10)
+  end
+
+  it 'should raise an error if topping up would exceed limit' do
+    expect { card.top_up(100) }.to raise_error("Balance can\'t be more than #{Oystercard::DEFAULT_MAXIMUM}")
   end
 
 end
